41d84be4f9b2c3d54958c6d688ebbda9b6788227
Heather Barr <hnbarr@yahoo.com>
2019-04-11 20:24:52 -0500

    lodash not working bc require done here

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// import _ from 'lodash';
+// let lodash = require('lodash')
+
+const names = ['Heather', 'Michael', 'Jim', 'Dale', 'Emilio', 'Amber']
+const colors = ['blue', 'red', 'pink', 'yellow', 'orange', 'purple', 'green']
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const objects = [
+    {
+        name: 'Chris',
+        age: 29,
+    },
+
+    {
+        name: 'John',
+        age: 25,
+    },
+
+    {
+        name: 'Joann',
+        age: 23,
+    },
+
+    {
+        name: 'Megan',
+        age: 28,
+    },
+]
+
+document.getElementById('chunkArr')
+
